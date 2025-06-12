edf9eda59a771cdbc7615b07c6bdf5c1f9b445f2
shiva.avula <shiva.avula@medplusindia.com>
2025-06-12 16:33:13 +0530

    changes

--- src/meplus/MainClass.java
+++ src/meplus/MainClass.java
@@ -3,7 +3,9 @@ package meplus;
 public class MainClass {
 
 	public static void main(String[] args) {
-
+		Sysout.println("Hello, MePlus!");
+		Sysout.println("This is a simple Java application.");
+		Sysout.println("You can add your own functionality here.");
 	}
 
 }
