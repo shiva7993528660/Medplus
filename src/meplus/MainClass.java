b380b790c3955422d2cbb17508c10d0f495d1c45
shiva.avula <shiva.avula@medplusindia.com>
2025-06-12 18:14:03 +0530

    commit

--- src/meplus/MainClass.java
+++ src/meplus/MainClass.java
@@ -3,7 +3,7 @@ package meplus;
 public class MainClass {
 
 	public static void main(String[] args) {
-		Sysout.println("Hello, MePlus!");
+		Sysout.println("Hello, MePlusnbsx ja jh x!");
 		Sysout.println("This is a simple Java application.");
 		Sysout.println("You can add your own functionality here.");
 	}
